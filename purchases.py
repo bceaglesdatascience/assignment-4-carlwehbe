2870c535a44ea356100473a7a5f0b46ffe311131
carlwehbe <wehbeca@bc.edu>
2023-09-21 20:43:01 -0400

    Update purchases.py

---
+++ purchases.py
@@ -0,0 +1,25 @@
+purchases = float(input("Number of Purchases: "))
+tax = float(input("Sales Tax: "))
+names = []
+costs = []
+i = 0
+while i < purchases:
+    cust = input("Customer: ")
+    names.append(cust)
+    cost = float(input("Cost: "))
+    costs.append(cost)
+    i+=1
+def add_tax(cost_list, salestax):
+    n_costs = []
+    for cost in cost_list:
+        n_cost = cost * (salestax+1)
+        n_costs.append(n_cost)
+    return(n_costs)
+n_costs = add_tax(costs, tax)
+ans = {}
+for i in range(len(n_costs)):
+    if names[i] in ans:
+        ans[names[i]] += n_costs[i]
+    else:
+        ans[names[i]] = n_costs[i]
+print(ans)
